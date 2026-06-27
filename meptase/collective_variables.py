fc4822d249a9d049a081ee93957ff65472b5598a
fazekaszs <zsolt.fazekas.9@gmail.com>
2026-06-27 20:07:25 +0200

    Added docstrings to the CV classes. Renamed some variables for clarity. The class DihedralCV now operates on dihedral angle batches.

--- meptase/collective_variables.py
+++ meptase/collective_variables.py
@@ -3,9 +3,34 @@ import torch.nn as nn
 
 EPSILON = 1E-8
 
+
+class MergeCV(nn.Module):
+    """
+    Calculates and merges multiple collective variable vectors through simple concatenation.
+    """
+
+    def __init__(self, cv_mappers: list[nn.Module]):
+        super().__init__()
+        self.cv_mappers = cv_mappers
+
+    def forward(self, positions: torch.Tensor) -> torch.Tensor:
+
+        cv_list = [mapper(positions) for mapper in self.cv_mappers]
+        return torch.concat(cv_list, dim=0)
+
+
 class DistanceCV(nn.Module):
+    """
+    Calculates the distance between coordinate pairs given by their indices in the coordinate matrix.
+    """
 
     def __init__(self, indices: torch.Tensor):
+        """
+        Class constructor.
+
+        :param indices: An indexing tensor containing integers that has a shape of (M, 2), where
+            M is the number of distances to be calculated.
+        """
         super().__init__()
         self.indices = indices
 
@@ -17,16 +42,25 @@ class DistanceCV(nn.Module):
         #     coordinate triplet pairs: [p1_m p2_m] = [[p1_xm p1_ym p1_zm] [p2_xm p2_ym p2_zm]],
         #     where m: 0...M-1.
 
-        triplet_pairs = positions[self.indices]
-        deltas = triplet_pairs[:, 0, :] - triplet_pairs[:, 1, :]  # (M, 3)
+        vector_pairs = positions[self.indices]
+        deltas = vector_pairs[:, 0, :] - vector_pairs[:, 1, :]  # (M, 3)
         distances = torch.sqrt(torch.sum(deltas ** 2, dim=1) + EPSILON)  # (M, )
 
         return distances
 
 
 class AngleCV(nn.Module):
+    """
+    Calculates the angle between coordinate triplets given by their indices in the coordinate matrix.
+    """
 
     def __init__(self, indices: torch.Tensor):
+        """
+        Class constructor.
+
+        :param indices: An indexing tensor containing integers that has a shape of (M, 3), where
+            M is the number of angles to be calculated.
+        """
         super().__init__()
         self.indices = indices
 
@@ -40,12 +74,12 @@ class AngleCV(nn.Module):
         #     ],
         #     where m: 0...M-1.
 
-        triplets = positions[self.indices]
+        vector_triplets = positions[self.indices]
 
         # Vectorize displacement vectors from the central vertex (index 1 in dim 1)
         # Both v_ba and v_bc will have a shape of (M, 3)
-        v_ba = triplets[:, 0, :] - triplets[:, 1, :]
-        v_bc = triplets[:, 2, :] - triplets[:, 1, :]
+        v_ba = vector_triplets[:, 0, :] - vector_triplets[:, 1, :]
+        v_bc = vector_triplets[:, 2, :] - vector_triplets[:, 1, :]
 
         # Batch reduce norms along the spatial axis (dim=1) -> shape (M,)
         norm_ba = torch.sqrt(torch.sum(v_ba ** 2, dim=1) + EPSILON)
@@ -62,28 +96,53 @@ class AngleCV(nn.Module):
 
 
 class DihedralCV(nn.Module):
+    """
+    Calculates the dihedral angle between coordinate quadruplets given by their indices in the coordinate matrix.
+    """
 
-    def __init__(self, idx_a: int, idx_b: int, idx_c: int, idx_d: int):
+    def __init__(self, indices: torch.Tensor):
+        """
+        Class constructor.
+
+        :param indices: An indexing tensor containing integers that has a shape of (M, 4), where
+            M is the number of dihedral angles to be calculated.
+        """
         super().__init__()
-        self.idx_a = idx_a
-        self.idx_b = idx_b
-        self.idx_c = idx_c
-        self.idx_d = idx_d
+        self.indices = indices
 
     def forward(self, positions: torch.Tensor) -> torch.Tensor:
-        b1 = positions[self.idx_b] - positions[self.idx_a]
-        b2 = positions[self.idx_c] - positions[self.idx_b]
-        b3 = positions[self.idx_d] - positions[self.idx_c]
 
+        # - positions is (N, 3), where N denotes the number of atoms
+        # - self.indices is (M, 4), where M denotes the number of angles to calculate
+        # - positions[self.indices] is a tensor of shape (M, 4, 3), which contains the
+        #     coordinate triplet quadruplets: [p1_m p2_m p3_m p4_m] = [
+        #       [p1_xm p1_ym p1_zm] [p2_xm p2_ym p2_zm] [p3_xm p3_ym p3_zm] [p4_xm p4_ym p4_zm]
+        #     ],
+        #     where m: 0...M-1.
+
+        vector_quadruplets = positions[self.indices]
+
+        # Batch extract bond displacement vectors -> shape (M, 3)
+        b1 = vector_quadruplets[:, 1, :] - vector_quadruplets[:, 0, :]
+        b2 = vector_quadruplets[:, 2, :] - vector_quadruplets[:, 1, :]
+        b3 = vector_quadruplets[:, 3, :] - vector_quadruplets[:, 2, :]
+
+        # Batch cross product to find plane normals
+        # torch.linalg.cross automatically evaluates over the last, spatial dimension
         n1 = torch.linalg.cross(b1, b2)
         n2 = torch.linalg.cross(b2, b3)
 
-        n1 = n1 / torch.sqrt(torch.sum(n1 ** 2) + EPSILON)
-        n2 = n2 / torch.sqrt(torch.sum(n2 ** 2) + EPSILON)
-        b2_norm = b2 / torch.sqrt(torch.sum(b2 ** 2) + EPSILON)
+        # Batch normalize the vectors
+        n1 = n1 / torch.sqrt(torch.sum(n1 ** 2, dim=1, keepdim=True) + EPSILON)
+        n2 = n2 / torch.sqrt(torch.sum(n2 ** 2, dim=1, keepdim=True) + EPSILON)
 
-        x = torch.sum(n1 * n2)
+        b2_norm = b2 / torch.sqrt(torch.sum(b2 ** 2, dim=1, keepdim=True) + EPSILON)
+
+        # Project orthogonal components
+        x = torch.sum(n1 * n2, dim=1)
+
         m1 = torch.linalg.cross(n1, b2_norm)
-        y = torch.sum(m1 * n2)
+        y = torch.sum(m1 * n2, dim=1)
 
+        # Batch evaluation of atan2 returns (M,) array containing full periodic values
         return torch.atan2(y, x)
