Fixed a backprop bug; at the beginning, when no hills have been deposited yet, the bias potential in a MergedPEF is the constant zero, independent of the coordinates. Same goes for the bias potential. This creates two problems; the constant potentials require grad (and now they do have grad) AND PyTorch needs to materialize 0 gradients (if it does not do that, it will be None). At first, allow_unused in torch.autograd.grad seemed like a good solution, but it still returns None (it just allows disconnected tensors to be evaluated in the gradient calculation tree, but does not result in a materialized 0 grad).

meptase/additional_potentials.py
@@ -93,7 +93,7 @@ class MergedPEF(PotentialEnergyFunction):
     def run(self, current_cv: torch.Tensor) -> torch.Tensor:
         return sum(
             (pef(current_cv) for pef in self.potential_energy_functions),
-            start=torch.zeros(tuple())
+            start=torch.zeros(tuple(), requires_grad=True, )
         )
 
 

meptase/metadynamics.py
@@ -108,7 +108,7 @@ class MetaDynamicsEngine:
             metadynamics_potentials = self.kernel_height * torch.sum(torch.prod(densities, dim=2), dim=1)
 
         else:
-            metadynamics_potentials = torch.zeros(size=(current_cv.shape[0], ))
+            metadynamics_potentials = torch.zeros(size=(current_cv.shape[0], ), requires_grad=True)
 
         total_potential = additional_potential + metadynamics_potentials
 
@@ -119,7 +119,8 @@ class MetaDynamicsEngine:
             forces = -1. * torch.autograd.grad(
                 outputs=total_potential,
                 inputs=coordinates,
-                grad_outputs=torch.ones_like(total_potential)
+                grad_outputs=torch.ones_like(total_potential),
+                materialize_grads=True
             )[0]
 
         return total_potential, forces